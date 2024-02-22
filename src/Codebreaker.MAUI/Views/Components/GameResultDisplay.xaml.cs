dc075a5659df9e0df9792f33c0f7c8ed2cb89d13
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-02-22 12:59:52 +0100

    Use VSM instead of IsVisible property for MAUI

--- src/Codebreaker.MAUI/Views/Components/GameResultDisplay.xaml.cs
+++ src/Codebreaker.MAUI/Views/Components/GameResultDisplay.xaml.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 namespace Codebreaker.MAUI.Views.Components;
 
 public partial class GameResultDisplay : ContentView
@@ -5,5 +7,31 @@ public partial class GameResultDisplay : ContentView
 	public GameResultDisplay()
     {
         InitializeComponent();
+        Loaded += OnLoaded;
+        GoToState("Default");
     }
+
+    public GamePageViewModel ViewModel => (GamePageViewModel)BindingContext;
+
+    private void OnLoaded(object? sender, EventArgs e)
+    {
+        ViewModel.PropertyChanged += OnViewModelPropertyChanged;
+    }
+
+    private void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs args)
+    {
+        if (args.PropertyName != nameof(GamePageViewModel.GameStatus))
+            return;
+
+        var stateName = ViewModel.GameStatus switch
+        {
+            GameMode.Won => "Won",
+            GameMode.Lost => "Lost",
+            _ => "Default"
+        };
+        GoToState(stateName);
+    }
+
+    private void GoToState(string stateName) =>
+        VisualStateManager.GoToState(this, stateName);
 }
