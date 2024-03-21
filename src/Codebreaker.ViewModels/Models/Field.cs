4b1a4e05d23abeb3abe44d794ec21c5db2ba3c96
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-03-21 12:28:32 +0100

    Added documentation headers in Field.cs

--- src/Codebreaker.ViewModels/Models/Field.cs
+++ src/Codebreaker.ViewModels/Models/Field.cs
@@ -1,9 +1,19 @@
 ﻿namespace Codebreaker.ViewModels.Models;
 
+/// <summary>
+/// Represents a field in a move.
+/// </summary>
+/// <param name="possibleColors">The possible colors for this field.</param>
 public partial class Field(string[] possibleColors) : ObservableObject
 {
+    /// <summary>
+    /// The selected color for this field.
+    /// </summary>
     [ObservableProperty]
     private string? _color;
 
+    /// <summary>
+    /// The possible colors for this field.
+    /// </summary>
     public string[] PossibleColors { get; init; } = possibleColors;
 }
