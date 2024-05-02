e041716ff13d901a11a9ae3eeaa2f4eaf29be393
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-05-03 01:22:35 +0200

    Adapted GameResultDisplay for the new vm lib. It now reacts to GameEndedMessages.

--- src/Codebreaker.WinUI/Views/Components/GameResultDisplay.xaml.cs
+++ src/Codebreaker.WinUI/Views/Components/GameResultDisplay.xaml.cs
@@ -1,12 +1,15 @@
 using Codebreaker.ViewModels;
-using System.ComponentModel;
+using Codebreaker.ViewModels.Messages;
+using CommunityToolkit.Mvvm.Messaging;
 
 namespace CodeBreaker.WinUI.Views.Components;
 
-public sealed partial class GameResultDisplay : UserControl
+public sealed partial class GameResultDisplay : UserControl, IRecipient<GameEndedMessage>
 {
     public GameResultDisplay()
     {
+        WeakReferenceMessenger.Default.RegisterAll(this);
+        WeakReferenceMessenger.Default.UnregisterAllOnUnloaded(this);
         InitializeComponent();
         this.GoToState("Default", false);
     }
@@ -18,25 +21,8 @@ public sealed partial class GameResultDisplay : UserControl
     }
 
     public static readonly DependencyProperty ViewModelProperty =
-        DependencyProperty.Register(nameof(ViewModel), typeof(GamePageViewModel), typeof(GameResultDisplay), new PropertyMetadata(null, OnViewModelChanged));
+        DependencyProperty.Register(nameof(ViewModel), typeof(GamePageViewModel), typeof(GameResultDisplay), new PropertyMetadata(null));
 
-    private static void OnViewModelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-    {
-        var @this = (GameResultDisplay)d;
-        @this.ViewModel.PropertyChanged += @this.OnViewModelPropertyChanged;
-    }
-
-    private void OnViewModelPropertyChanged(object? sender, PropertyChangedEventArgs args)
-    {
-        if (args.PropertyName != nameof(GamePageViewModel.GameStatus))
-            return;
-
-        var stateName = ViewModel.GameStatus switch
-        {
-            GameMode.Won => "Won",
-            GameMode.Lost => "Lost",
-            _ => "Default"
-        };
-        this.GoToState(stateName);
-    }
+    public void Receive(GameEndedMessage message) =>
+        this.GoToState(message.IsVictory ? "Won" : "Lost");
 }
