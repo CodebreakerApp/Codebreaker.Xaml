7f6660d208409030b514444c1491566dcd741cef
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-05-19 18:54:24 +0200

    Removed code for old Codebreaker.Uno

--- src/Codebreaker.Uno/CodebreakerUno/Helpers/ResourceExtensions.cs
+++
@@ -1,8 +0,0 @@
-namespace CodeBreaker.Uno.Helpers;
-
-internal static class ResourceExtensions
-{
-    private static readonly IStringLocalizer s_stringLocalizer = App.GetService<IStringLocalizer>();
-
-    public static string GetLocalized(this string resourceKey) => s_stringLocalizer.GetString(resourceKey);
-}
--- src/Codebreaker.Uno/CodebreakerUno/Helpers/VisualStateHelpers.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Microsoft.UI.Xaml;
-
-internal static class VisualStateHelpers
-{
-    public static bool GoToState(this Control control, string stateName, bool useAnimation = true) =>
-        VisualStateManager.GoToState(control, stateName, useAnimation);
-}
