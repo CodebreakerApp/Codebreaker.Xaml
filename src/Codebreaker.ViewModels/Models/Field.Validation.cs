717c24ed5fba9b20b73d5dee5e224608c307459c
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-07-08 13:28:04 +0200

    Added partial class for field for validation purposes

---
+++ src/Codebreaker.ViewModels/Models/Field.Validation.cs
@@ -0,0 +1,8 @@
+﻿namespace Codebreaker.ViewModels.Models;
+
+public partial class Field
+{
+    public bool IsSet =>
+        PossibleColors is null || Color is not null &&
+        PossibleShapes is null || Shape is not null;
+}
