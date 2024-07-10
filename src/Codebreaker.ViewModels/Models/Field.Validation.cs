027d25ac630fcc0a043c34688ac9e1ffb8603af1
Sebastian Szvetecz <sebastian@szvetecz.at>
2024-07-10 14:46:16 +0200

    Removed validation partial class for Field

--- src/Codebreaker.ViewModels/Models/Field.Validation.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Codebreaker.ViewModels.Models;
-
-public partial class Field
-{
-    public bool IsSet =>
-        PossibleColors is null || Color is not null &&
-        PossibleShapes is null || Shape is not null;
-}
